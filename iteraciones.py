47e2c29fd0ab6a1a7695c37c9d0dd05cd0d4c88c
Jeffrey Smith Bustamante <jeffreyrsb2@gmail.com>
2020-09-06 14:39:08 -0500

    iteraciones

---
+++ iteraciones.py
@@ -0,0 +1,30 @@
+# horas = 0
+# minutos = 0
+
+# while horas < 24:
+#     while minutos < 60:
+#         print (str(horas) + " : " + str(minutos))
+#         minutos += 1
+#     horas +=1
+#     minutos = 0
+
+# x = 0.0
+# for i in range(10):
+#     print(str(x))
+#     x += 0.1
+
+# if x == 1.0:
+#     print(f'x = {x}')
+# else:
+#     print(f'x != {x}')
+
+# x = 0.0
+# for i in range(10):
+#     x += 0.1
+
+# x = round(x, 1)
+
+# if x == 1.0:
+#     print(f'x = {x}')
+# else:
+#     print(f'x != {x}')
