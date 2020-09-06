c2940b61445a5102ed795b0c49c698d527314513
Jeffrey Smith Bustamante <jeffreyrsb2@gmail.com>
2020-09-06 14:39:47 -0500

    iteraciones2

--- iteraciones.py
+++ iteraciones.py
@@ -1,17 +1,17 @@
-# horas = 0
-# minutos = 0
+horas = 0
+minutos = 0
 
-# while horas < 24:
-#     while minutos < 60:
-#         print (str(horas) + " : " + str(minutos))
-#         minutos += 1
-#     horas +=1
-#     minutos = 0
+while horas < 24:
+    while minutos < 60:
+        print (str(horas) + " : " + str(minutos))
+        minutos += 1
+    horas +=1
+    minutos = 0
 
-# x = 0.0
-# for i in range(10):
-#     print(str(x))
-#     x += 0.1
+x = 0.0
+for i in range(10):
+    print(str(x))
+    x += 0.1
 
 # if x == 1.0:
 #     print(f'x = {x}')
